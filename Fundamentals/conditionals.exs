cfdc8acff643398c8dfb5a4e0d8c837aa0b62cff
Yuri Melo <yurifullstack@gmail.com>
2023-05-20 00:47:55 -0300

    Conditionals

--- Fundamentals/conditionals.exs
+++ Fundamentals/conditionals.exs
@@ -18,3 +18,19 @@ end
 
 IO.puts f.(1, 3)
 IO.puts f.(-1, 3)
+
+
+# Cond
+#case is useful when you need to match against different values. However, in many circumstances, we want to check different conditions and find the first one that does not evaluate to nil or false. In such cases, one may use cond:
+cond do
+  2 + 2 == 5 ->
+    IO.puts "This will not be true"
+  2 * 3 == 1 ->
+    IO.puts "Nor this"
+  :ok == :networkerror ->
+    IO.puts "Nor me"
+  :success == :successw ->
+    IO.puts "But this will"
+  true ->
+    IO.puts "This is always true (equivalent to else)"
+end
